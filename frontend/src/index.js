438a934e1c7be4b02566d13467215be6a471c50d
mancinidsb <guilhermemancinidsb@gmail.com>
2024-11-12 14:02:16 -0300

    feature: integra frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,7 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import App from "./App";
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
+root.render(<App/>)
